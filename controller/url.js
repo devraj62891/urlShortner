978c61f51fc94dc5e5551a8bec309953ba20b6a4
Sharma <2463455@cognizant.com>
2026-02-24 17:39:56 +0530

    done withe ejs setup

--- controller/url.js
+++ controller/url.js
@@ -14,8 +14,11 @@ async function handleGenerateNewShortURL(req,res){
         visitHistory:[],
     }
  );
+  
+ return res.render("home",{
+    id:shortID
+ })
 
- return res.json({id: shortID});
 }
 
 async function handleRedirectRequest(req,res){
